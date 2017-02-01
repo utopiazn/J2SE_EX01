a8970f30b0c9e499975176634b99e4f9394f46f3
user1 <user1@KH_I>
2017-02-01 15:07:23 +0900

    연습문제

---
+++ J2SE/src/PracticeProblem/Ex06/Ex06_1.java
@@ -0,0 +1,24 @@
+package PracticeProblem.Ex06;
+
+import PracticeProblem.*; 
+
+
+/*
+class SytdaCard01{
+	
+	int num;  // 카드의 숫자.(1~10사이의 정수)
+	boolean isKwang;  // 광이면 :True.아니면 false	
+	
+	
+}*/
+
+public class Ex06_1 {
+	
+	public static void main(String[] args){
+		
+		//SytdaCard sc = new SytdaCard();
+		//System.out.println(sc.num+ " :: " +sc.isKwang);
+	}
+
+
+}
---
+++ J2SE/src/PracticeProblem/Ex06/Ex06_2.java
@@ -0,0 +1,18 @@
+package PracticeProblem.Ex06;
+
+class SytdaCard2{
+	
+	int num;  // 카드의 숫자.(1~10사이의 정수)
+	boolean isKwang;  // 광이면 :True.아니면 false	
+	
+	
+}
+
+public class Ex06_2 {
+
+	public static void main(String[] args){
+		
+		
+		System.out.println();
+	}
+}
