2f8375881affeb0aafa63dd770e1c11204ca302d
user1 <user1@KH_I>
2017-02-16 15:16:12 +0900

    ㅇㄹ

--- J2SE/src/feb16/RandomAccessFileEx.java
+++ J2SE/src/feb16/RandomAccessFileEx.java
@@ -1,5 +1,22 @@
 package feb16;
 
+import java.io.*;
+
 public class RandomAccessFileEx {
+	
+	
+	
+	public static void main(String[] args){
+		
+		RandomAccessFile ra = null;
+		/*try{
+			ra = new RandomAccessFile("C:\\io\\random.txt", "rw");
+			
+			
+		}
+		*/
+		
+		
+	}
 
 }
